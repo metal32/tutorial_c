5a5904c0526051e6640d567201870649742f843c
metal32 <ayus097836@gmail.com>
2020-11-16 20:22:35 +0530

    Added examples where we are using keyword

---
+++ using_example.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Base {
+   public:
+      void greet() {
+         cout << "Hello from Base" << endl;;
+      }
+};
+class Derived : protected Base {
+   public:
+      // void greet(string s) {
+      //    cout << "Hello from " << s << endl;
+      //    // Instead of recursing, the greet() method
+      //    // of the base class is called.
+      //    Base::greet();
+      // }
+      using Base::greet;
+};
+int main() {
+   Derived D;
+   D.greet();
+   // D.greet("Derived");
+   return 0;
+}
