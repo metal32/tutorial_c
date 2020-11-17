6e94adb8f6a45e50342073421d35c96c03f393de
metal32 <ayus097836@gmail.com>
2020-11-17 11:22:19 +0530

    More bug fixes

--- RAII_implementation/normal_implementation/move_semantics.cpp
+++ RAII_implementation/normal_implementation/move_semantics.cpp
@@ -77,72 +77,12 @@ class Holder{
 		size_t m_size;
 };
 
-class HolderNewGen{
-	
-	public:
 
-		friend void swap(HolderNewGen& first, HolderNewGen& second) // nothrow
-	    {
-	        // enable ADL (not necessary in our case, but good practice)
-	        using std::swap;
-
-	        // by swapping the members of two objects,
-	        // the two objects are effectively swapped
-	        swap(first.m_size, second.m_size);
-	        swap(first.m_data, second.m_data);
-	    }
-
-	    HolderNewGen() {}
-
-		HolderNewGen(int size) {
-			m_size = size;
-			m_data = new int[size];
-		}
-
-		HolderNewGen(const HolderNewGen& other) {
-			m_data = new int[other.m_size];
-			std::copy(other.m_data, other.m_data+other.m_size, m_data);
-			m_size = other.m_size;
-		}
-
-		//Move constructor
-		HolderNewGen(HolderNewGen&& other) noexcept {
-			HolderNewGen();
-			swap(*this, other);
-		}
-
-		//Move assignment operator
-		HolderNewGen& operator=(HolderNewGen&& other) noexcept {
-			swap(*this, other);
-		}
-
-		//Copy and Swap Idiom way of writing copy assignment operator
-		//1) Get the copy of it and let compiler do it as it is the best platform to do it
-		// If copy fails then we will never enter the assignment function and hence our *this will be safe
-		//2) We need to have a swap function as created
-		HolderNewGen& operator=(HolderNewGen other){
-   			swap(*this, other);
-    		return *this;
-		}
-
-		~HolderNewGen() {
-			delete[] m_data;
-		}
-
-	private:
-		int* m_data;
-		size_t m_size;
-};
 
 // Now in this function is returning by value
 Holder createHolder(int size)
 {
  	return Holder(size);
-}
-
-HolderNewGen createNewGenHolder(int size)
-{
- 	return HolderNewGen(size);
 }
 
 int main() {
@@ -169,16 +109,6 @@ int main() {
 		Holder h3 = createHolder(2000); // move constructor (rvalue in input) (1) 
 		h2 = h3;                        // assignment operator (lvalue in input)
 	  	h2 = createHolder(500);         // move assignment operator (rvalue in input)
-	}
-
-	{
-		// Using Copy-and-idioms
-		HolderNewGen h1(10000); // regular constructor
-		HolderNewGen h2 = h1;   // copy constructor
-
-		HolderNewGen h5(10000);  // regular constructor
-		HolderNewGen h6(60000);  // regular constructor
-		h5 = h6;
 	}
 	
 	return 0;
